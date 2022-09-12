723355545fcd360819d6e8f9217716c05555e8e3
PranavParitshith <113255405+PranavParitshith@users.noreply.github.com>
2022-09-12 23:17:31 +0530

    Update DS8.c
    Linear Search

--- Programs/DS8.c
+++ Programs/DS8.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	int a[100],s,n,i,flag = 0,mid,high,low;
+	int a[100],s,n,i,flag = 0;
 	printf("Enter the Number of Elements : ");
 	scanf("%d",&n);
 	for(i = 0;i < n;i++){
@@ -10,21 +10,18 @@ int main(){
 	printf("\n");
 	printf("Enter the Element you want to Search For : ");
 	scanf("%d",&s);
-	high = n;
-	low = 1;
-	mid = (high+low)/2;
-	while(s != a[mid]){
-		if(s <= a[mid]){
-			low = 1;
-			high = mid + 1;
-			mid = (low + high)/2;
+	for(i = 0; i< n; i++){
+		if(s == a[i]){
+			printf("The Element %d is found at index %d\n",s,i);
+			flag = 0;
+			break;
 		}
 		else{
-			low = mid + 1;
-			high = n;
-			mid = (low+high)/2;
+			flag = 1;
 		}
 	}
-	printf("The Element %d is found at index %d",s,mid);
+	if(flag == 1){
+		printf("Not Found!\n");
+	}
 	return 0;
 }
